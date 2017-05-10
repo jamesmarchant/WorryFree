5f9cf7df4d8dd61f4f2c954504da2aed046bd024
jnmarchant <jamesmarchant@thedatashed.co.uk>
2017-05-10 15:10:28 +0100

    added worry-list component and more setup

--- app/routes/my-worries.js
+++ app/routes/my-worries.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return ['Death', 'Buying Train Tickets', 'Donald Trump'];
+        return [{
+          id: 'uuidrandom',
+          type: 'hypothetical',
+          title: 'People not liking me',
+          description: 'I dont know if people like me or not',
+          date: Date.now(),
+        },
+        {
+          id: 'uuidrandom1',
+          type: 'real',
+          title: 'Not Eating Healthy',
+          description: 'I dont think I eat healty food',
+          date: Date.now(),
+          deadline: new Date(Date.now() + (1000 /*sec*/ * 60 /*min*/ * 60 /*hour*/ * 24 /*day*/ * 10))
+        }];
    }
 });
---
+++ mirage/config.js
@@ -0,0 +1,51 @@
+// import uuid from 'uuid';
+
+export default function() {
+
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+
+    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
+  */
+  this.namespace = '/api';
+
+  this.get('/rentals', function() {
+    return {
+      data: [
+        {
+          id: 'uuidrandom',
+          type: 'hypothetical',
+          title: 'People not liking me',
+          description: 'I dont know if people like me or not',
+          date: Date.now(),
+        },
+        {
+          id: 'uuidrandom1',
+          type: 'real',
+          title: 'Not Eating Healthy',
+          description: 'I dont think I eat healty food',
+          date: Date.now(),
+          deadline: new Date(Date.now() + (1000 /*sec*/ * 60 /*min*/ * 60 /*hour*/ * 24 /*day*/ * 10))
+        }
+      ]
+    };
+  });
+}
