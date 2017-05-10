563970fee499ee832208d968589501ed1c86501d
jnmarchant <jamesmarchant@thedatashed.co.uk>
2017-05-10 15:42:44 +0100

    added classes and tests to worry-item

--- tests/integration/components/worry-item-test.js
+++ tests/integration/components/worry-item-test.js
@@ -1,5 +1,13 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+let worry = Ember.Object.create({
+  title: 'test-title',
+  type: 'real',
+  description: "test-description",
+  date: Date.now()
+});
 
 moduleForComponent('worry-item', 'Integration | Component | worry item', {
   integration: true
@@ -9,17 +17,10 @@ test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('worryObj', worry)
+  this.render(hbs`{{worry-item worry=worryObj}}`);
 
-  this.render(hbs`{{worry-item}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#worry-item}}
-      template block text
-    {{/worry-item}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.item .item-title').text(), 'test-title', 'test-title');
+  assert.equal(this.$('.item .item-type').text().trim(), 'real');
+  assert.equal(this.$('.item .text-description').text().trim(), 'test-description');
 });
